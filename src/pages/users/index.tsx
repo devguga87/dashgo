de09ed6efb098699a39bdfea7dc3ef1818a113de
Gustavo Seabra <devguga87@gmail.com>
2021-11-16 12:13:30 -0300

    feature: users pages

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Text ,Flex, Heading, Button, Icon, Table, Thead, Tbody, Tr, Td, Th , Checkbox} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
 
 export default function UserList(){
@@ -89,6 +90,7 @@ export default function UserList(){
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
